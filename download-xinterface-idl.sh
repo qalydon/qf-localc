83f22e316f8abdd26bfda9405060272e9fed9d01
qalydon <qalydon17@gmail.com>
2026-04-30 15:45:09 -0500

    Create a shell script to download the LO XInterface.idl file

---
+++ download-xinterface-idl.sh
@@ -0,0 +1,4 @@
+# Download the XInterface.idl file directly from the LibreOffice repo
+cd idl
+curl -L https://raw.githubusercontent.com/LibreOffice/core/refs/heads/master/udkapi/com/sun/star/uno/XInterface.idl -o XInterface.idl
+cd ..
